4cd80961c48ca2cda563cd8ee639683f5bd3ee33
illaweb35 <pierre.durand@domaine.fr>
2019-03-09 16:54:14 +0100

    create CollectionType Tickets

---
+++ src/Form/CollectionTicketsType.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Resa;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
+
+class CollectionTicketsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('tickets', CollectionType::class, [
+                'entry_type'    => TicketType::class,
+                'allow_add'     => true,
+                'allow_delete'  => true,
+                'prototype'     => true,
+                'by_reference'  => false
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Resa::class,
+        ]);
+    }
+}
