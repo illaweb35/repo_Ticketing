1f66b3cedb69d0f50624c25115b30571bdd36d3e
illaweb35 <pierre.durand@domaine.fr>
2019-03-13 14:13:28 +0100

    Modifs Homecontroller test

--- Tests/Controller/HomeControllerTest.php
+++ Tests/Controller/HomeControllerTest.php
@@ -2,6 +2,7 @@
 namespace App\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use App\Service\CalculateHolidays;
 
 class HomeControllerTest extends WebTestCase
 {
@@ -11,8 +12,6 @@ class HomeControllerTest extends WebTestCase
     *
     * @param url $url
     * @dataProvider urlProvider
-    * @return void
-    *
     */
     public function index($url)
     {
@@ -24,5 +23,34 @@ class HomeControllerTest extends WebTestCase
     public function urlProvider()
     {
         yield ['/'];
+    }
+
+    /**
+     * @test
+     *
+     * @param CalculateHolidays $holidays
+     * @return void
+     * @dataProvider dataHolidays
+     */
+    public function infos($testHolidays)
+    {
+        $holiday = new CalculateHolidays();
+
+        $year = new \Datetime();
+        $daysOff = $holiday->dayOff($year);
+        $holidays = $holiday->getHolidays($year);
+
+        $this->assertSame($testHolidays, $daysOff);
+    }
+    public function dataHolidays()
+    {
+        $year = new \Datetime();
+        return array(
+            [mktime(0, 0, 0, 1, 1, $year)],
+            [mktime(0, 0, 0, 5, 1, $year)],
+            [mktime(0, 0, 0, 07, 14, $year)],
+            [mktime(0, 0, 0, 11, 1, $year)],
+            [mktime(0, 0, 0, 12, 25, $year)]
+        );
     }
 }
